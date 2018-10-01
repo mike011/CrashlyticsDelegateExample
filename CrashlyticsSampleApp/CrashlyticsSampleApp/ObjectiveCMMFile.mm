01775a6625926f956e7a2270768cc7890de458a5
Michael Charland <michael.charland@textnow.com>
2018-10-01 19:48:20 -0400

    initial commit

---
+++ CrashlyticsSampleApp/CrashlyticsSampleApp/ObjectiveCMMFile.mm
@@ -0,0 +1,11 @@
+//
+//  ObjectiveCMMFile.mm
+//  CrashlyticsSampleApp
+//
+//  Created by Michael Charland on 2018-10-01.
+//  Copyright © 2018 enflick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "CrashlyticsSampleApp-Swift.h"
---
+++ CrashlyticsSampleApp/Pods/Target Support Files/Pods-CrashlyticsSampleApp/Pods-CrashlyticsSampleApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CrashlyticsSampleApp : NSObject
+@end
+@implementation PodsDummy_Pods_CrashlyticsSampleApp
+@end
